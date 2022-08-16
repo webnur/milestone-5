3e0a50cbe5f18eb3e8bf3016581cbb895c1c7a7e
webnur <mdnuruli291@gmail.com>
2022-08-16 10:52:15 +0600

    Update deposit and balance and handle string add

--- module 26 baper bank/js/deposit.js
+++ module 26 baper bank/js/deposit.js
@@ -4,13 +4,28 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     //step 2: get the deposit amout from the deposit input field
     // always remember to use .value to get text from an input field
     const depositField = document.getElementById('diposit-field');
-    const depositAmount = depositField.value;
-    // console.log(depositAmount)
-
+    // const newDepositAmount = depositField.value;
+    const newDepositAmount = parseFloat(depositField.value);
 
     // step 3: get the current deposit total 
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    // const previusDepositTotal = depositTotalElement.innerText;
+    const previusDepositTotal = parseFloat(depositTotalElement.innerText);
+
+    //step 4: add numbers to set the total deposit
+    const currentDeposit = previusDepositTotal + newDepositAmount
+    depositTotalElement.innerText = currentDeposit;
+
+    // step 5: get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText)
+
+    // step 6: calculate current total
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // step -7: clear the deposit field
+    depositField.value = '';
 
 })
