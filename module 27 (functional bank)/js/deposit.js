3259b347de791162777a98cd4348331a1df0cf6d
webnur <mdnuruli291@gmail.com>
2022-08-17 10:18:46 +0600

    Share one function between two input field

---
+++ module 27 (functional bank)/js/deposit.js
@@ -0,0 +1,24 @@
+
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value
+}
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    /* 
+    1. get the element by id
+    2. get the value from the element
+    3. convert string value a number 
+    */
+   const newDepositAmount = getInputFieldValueById('deposit-field')
+   console.log(newDepositAmount)
+})
