a760097afd5e641ef55a764ae5438bb90a225779
webnur <mdnuruli291@gmail.com>
2022-08-17 11:07:24 +0600

    Shared Balance update function done

--- module 27 (functional bank)/js/withdraw.js
+++ module 27 (functional bank)/js/withdraw.js
@@ -13,5 +13,15 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const totalWidthraw = previousWithdrawTotal + newWidthAmount
 
     // set withdraw total
-    setTextElementValueById('withdraw-total', totalWidthraw)
+    setTextElementValueById('withdraw-total', totalWidthraw);
+
+    // get previous balance 
+    const previousbalanceTotal = getElementValueById('balance-total');
+
+    // calculte new balance after withdraw
+    const newBalanceTotal = previousbalanceTotal - newWidthAmount;
+
+    // set new balance total
+    setTextElementValueById('balance-total', newBalanceTotal);
+
 })
