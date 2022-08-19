2b3c929e6b8e8d09b9d8018dbbc2abf32e4d44ce
webnur <mdnuruli291@gmail.com>
2022-08-19 09:52:51 +0600

    Pin matcher overview Generate pin and display it

---
+++ module 28 ( pin matcher )/js/app.js
@@ -0,0 +1,26 @@
+
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else {
+        // console.log('pin less then 4', pin)
+        return getPin()
+    }
+}
+
+function generatePin(){
+
+    const random = Math.round(Math.random()* 10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    // console.log(pin)
+
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField .value = pin;
+})
