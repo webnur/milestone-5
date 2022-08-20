0b1887abda240bfde734792f516db702028393f7
webnur <mdnuruli291@gmail.com>
2022-08-20 10:32:37 +0600

    done the project

--- module 28.5 ( money master )/js/app.js
+++ module 28.5 ( money master )/js/app.js
@@ -5,25 +5,25 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const incomeInputField = document.getElementById('income-input');
     const incomeValueString = incomeInputField.value;
     const incomeAmount = parseFloat(incomeValueString)
-    incomeInputField.value = '';
+    // incomeInputField.value = '';
 
     // food cose
     const foodCost = document.getElementById('food-cost');
     const foodCostString = foodCost.value;
     const totalFoodCost = parseFloat(foodCostString);
-    foodCost.value = '';
+    // foodCost.value = '';
 
     // rent cost
     const rentCost = document.getElementById('rent-cost');
     const rentCostString = rentCost.value;
     const totalRentCost = parseFloat(rentCostString);
-    rentCost.value = ''
+    // rentCost.value = ''
 
     // cloth price
     const clothPrice = document.getElementById('cloth-price');
     const clothpiceString = clothPrice.value;
     const totalClothPrice = parseFloat(clothpiceString);
-    clothPrice.value = '';
+    // clothPrice.value = '';
     
     // total-expenses
     const totalExpenses = totalFoodCost + totalRentCost + totalClothPrice; 
@@ -47,7 +47,7 @@ document.getElementById('save-button').addEventListener('click', function(){
     const incomeInputField = document.getElementById('income-input');
     const incomeValueString = incomeInputField.value;
     const incomeAmount = parseFloat(incomeValueString)
-    incomeInputField.value = '';
+    // incomeInputField.value = '';
 
     const savingAmountField = document.getElementById('save-amount-input');
     const savingString = savingAmountField.value;
@@ -59,5 +59,13 @@ document.getElementById('save-button').addEventListener('click', function(){
     const showSaveAmount = document.getElementById('save-amount-value');
 
     showSaveAmount.innerText = totalSave;
+
+    const totalExpensesElement = document.getElementById('total-expenses');
+    const totalExpenses = parseFloat(totalExpensesElement.innerText);
+
+    const totalRemainingBalance = incomeAmount - (totalExpenses + totalSave);
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = totalRemainingBalance;
+
 
 })
